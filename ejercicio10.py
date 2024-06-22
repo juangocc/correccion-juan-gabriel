5a11dd38529a18b106946533b2b123d4fb167488
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 15:48:50 -0500

    Corregido ejercicio 10 quitado identacion, puesta edad en cadena.

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,6 +1,5 @@
-
-10. ```python
-     nombre = "Juan"
-     apellido = "Pérez"
-     edad = 30
-     print("Nombre: " + nombre + ", Apellido: " + apellido + ", Edad: " + edad)
+# 10. python
+nombre = "Juan"
+apellido = "Pérez"
+edad = 30
+print("Nombre: " + nombre + ", Apellido: " + apellido + ", Edad: " + str(edad))
