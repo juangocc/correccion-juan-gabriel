ad0da67be1abfd8ae1f796c755161ae981cd8b3e
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 17:27:43 -0500

    Corregido ejercicio 18 colocado en cadena str.

--- ejercicio18.py
+++ ejercicio18.py
@@ -2,4 +2,4 @@ def encontrar_minimo(numeros):
     return min(numeros)
 
 numeros = [10, 5, 8, 3, 12]
-print("El número más pequeño es:", encontrar_minimo(numeros))
+print("El número más pequeño es : " + str(encontrar_minimo(numeros)))
