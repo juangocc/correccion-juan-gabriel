bcad1cba549731043a46a97c3f508016dac93364
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 15:47:08 -0500

    Corregido ejercicio 9 quitado identacion.

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,7 +1,7 @@
- def calcular_factorial(n):
-     factorial = 1
-     for i in range(1, n+1):
-         factorial *= i
-     return factorial
+def calcular_factorial(n):
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+    return factorial
 
- print(calcular_factorial(5))
+print(calcular_factorial(5))
