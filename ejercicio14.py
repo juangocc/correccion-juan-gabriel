56232efd213110220060fd7bb918c2a766018d28
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 17:08:33 -0500

    Corregido ejercicio 14 colocado funcion en cadena.

--- ejercicio14.py
+++ ejercicio14.py
@@ -4,4 +4,4 @@ def calcular_promedio(notas):
     return promedio
 
 notas = [85, 92, 78, 91, 88]
-print("El promedio de las notas es:", calcular_promedio(notas))
+print("El promedio de las notas es : " + str(calcular_promedio(notas)))
