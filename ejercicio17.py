d9e957f03c9c0360db062132abbc5491d1af10d4
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 17:25:37 -0500

    Corregido ejercicio 17 agregado comentario.

--- ejercicio17.py
+++ ejercicio17.py
@@ -1,7 +1,9 @@
+numero = 10
+print("=== Mostrar los "+str(numero)+" numeros de la secuencia fibonacci ===")
 def fibonacci(n):
     if n <= 1:
         return n
     else:
         return (fibonacci(n-1) + fibonacci(n-2))
 
-print([fibonacci(i) for i in range(10)])
+print([fibonacci(i) for i in range(numero)])
