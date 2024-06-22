ad1748e4c99e08c98b9b789edd95850f83341be0
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 15:34:38 -0500

    Corregido ejercicio 6 quitado identacion y agregado comentario.

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,10 +1,9 @@
-
-6. ```python
-    x = 10
-    y = 5
-    if x > y:
-        print("x es mayor que y")
-    elif x < y:
-        print("y es mayor que x")
-    else:
-        print("x es igual a y")
+#6. python
+x = 10
+y = 5
+if x > y:
+    print("x es mayor que y")
+elif x < y:
+    print("y es mayor que x")
+else:
+    print("x es igual a y")
