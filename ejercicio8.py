8b8a55408c758084da07237fdf532468890881d8
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 15:44:55 -0500

    Corregido ejercicio 8 quitado identacion agregado comentario.

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,8 +1,7 @@
-
-8. ```python
-    numeros = [1, 2, 3, 4, 5]
-    for num in numeros:
-        if num % 2 == 0:
-            print(num, "es par")
-        else:
-            print(num, "es impar")
+# 8. python
+numeros = [1, 2, 3, 4, 5]
+for num in numeros:
+    if num % 2 == 0:
+        print(num, "es par")
+    else:
+        print(num, "es impar")
