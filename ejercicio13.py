b7ac72dd9ffc4e4503e7fc95ba543c0e3b1c7cc9
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 16:07:55 -0500

    Corregido ejercicio 13 agregado parentesis potenciacion.

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,5 +1,5 @@
 def calcular_area_circulo(radio):
-    area = 3.14159 * radio ** 2
+    area = 3.14159 * (radio ** 2)
     return area
 
-print("El área del círculo es:", calcular_area_circulo(5))
+print("El área del círculo es : " + str(calcular_area_circulo(5)))
