ff6e76510f0452ace833ed9988cc7db3ffd0153f
Juan Gabriel Oyola Cardona <juangocc@gmail.com>
2024-06-22 16:00:43 -0500

    Corregido ejercicio 11 cambiado concatenacion de (,) por un (+).

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,7 +1,7 @@
 def es_palindromo(palabra):
     if palabra == palabra[::-1]:
-        print(palabra, "es un palíndromo")
+        print(palabra + " es un palíndromo")
     else:
-        print(palabra, "no es un palíndromo")
+        print(palabra + " no es un palíndromo")
 
 es_palindromo("radar")
